05b9847d4085867ad6ea1c664c5e701e352b02b8
Loïc Carbonne <loicc@theodo.fr>
2020-07-28 18:55:43 +0200

    feat: add sentry

--- packages/quicko-webapp/src/index.tsx
+++ packages/quicko-webapp/src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from "@sentry/apm";
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './shared/store';
 import FileLoader from './MainPage/FileLoader';
 import MainPage from './MainPage';
+
+Sentry.init({
+  dsn: 'https://dfc5dfbfd66d4e28a575c00e4512c77a@sentry.theo.do/4',
+  release: 'my-project-name@' + process.env.npm_package_version,
+  integrations: [
+    new Integrations.Tracing(),
+  ],
+  tracesSampleRate: 1.0, // Be sure to lower this in production
+});
 
 ReactDOM.render(
   <Provider store={store}>
